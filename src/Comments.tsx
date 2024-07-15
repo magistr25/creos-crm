cc98714322b7b950409b048cc3138457312484fd
Ekaterina Miakisheva <magistr25@inbox.ru>
2024-07-15 23:29:27 +0600

    formatDateDistanceDetailed

--- src/Comments.tsx
+++ src/Comments.tsx
@@ -1,7 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { formatDistanceToNow } from 'date-fns';
-import { ru } from 'date-fns/locale';
 
 interface Designer {
     avatar: string;
@@ -32,6 +30,47 @@ const fetchComments = async (): Promise<Comment[]> => {
     }
 };
 
+const formatDateDistanceDetailed = (createdAt: string) => {
+    const now = new Date();
+    const createdDate = new Date(createdAt);
+    const diff = Math.abs(now.getTime() - createdDate.getTime());
+    const seconds = Math.floor(diff / 1000);
+    const minutes = Math.floor(seconds / 60);
+    const hours = Math.floor(minutes / 60);
+    const days = Math.floor(hours / 24);
+    const formatted = [];
+    const getCorrectForm = (number: number, forms: [string, string, string]): string => {
+        const n = Math.abs(number) % 100;
+        const n1 = n % 10;
+        if (n > 10 && n < 20) {
+            return forms[2];
+        }
+        if (n1 > 1 && n1 < 5) {
+            return forms[1];
+        }
+        if (n1 === 1) {
+            return forms[0];
+        }
+        return forms[2];
+    };
+    if (minutes % 60 > 0) {
+        formatted.push(`${minutes % 60} ${getCorrectForm(minutes % 60, ['минута', 'минуты', 'минут'])}`);
+    }
+    if (hours % 24 > 0) {
+        formatted.push(`${hours % 24} ${getCorrectForm(hours % 24, ['час', 'часа', 'часов'])}`);
+    }
+    if (days > 0) {
+        formatted.push(`${days} ${getCorrectForm(days, ['день', 'дня', 'дней'])}`);
+    }
+    if (formatted.length === 0) {
+        return 'только что';
+    } else {
+        return formatted.join(' ') + ' назад';
+    }
+};
+
+
+
 const Comments: React.FC = () => {
     const [comments, setComments] = useState<Comment[]>([]);
 
@@ -53,10 +92,7 @@ const Comments: React.FC = () => {
                              style={{width: '50px', height: '50px', borderRadius: '50%', marginRight: '10px'}}/>
                         <div>
                             <div style={{fontWeight: 'bold'}}>{comment.designer.username}</div>
-                            <div style={{color: 'gray'}}>{formatDistanceToNow(new Date(comment.date_created), {
-                                addSuffix: true,
-                                locale: ru
-                            })}</div>
+                            <div style={{color: 'gray'}}>{formatDateDistanceDetailed(comment.date_created)}</div>
                         </div>
                     </div>
                     <div>
